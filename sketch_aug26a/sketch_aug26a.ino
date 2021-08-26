2c941b631230515518b87de5e9185a730f120332
AZMIRAI <1901322@g.yju.ac.kr>
2021-08-26 19:10:32 +0900

    arduino

--- sketch_aug26a/sketch_aug26a.ino
+++ sketch_aug26a/sketch_aug26a.ino
@@ -1,9 +1,11 @@
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200); //통신속도를 115200bps로 설정 1초에 1152문자를 보냄
+   pinMode(8,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hello, World!"); //문자열 출력
+  digitalWrite(8,HIGH);
+  delay(1000);
+  digitalWrite(8,LOW);
+  delay(1000);               
 }
